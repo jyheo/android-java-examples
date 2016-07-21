056fe72975ce53b10df5ebf210eb7955309014d4
Junyoung Heo <jyheo0@gmail.com>
2016-07-22 08:39:46 +0900

    material design test

---
+++ MaterialTest/app/src/main/java/com/example/jyheo/materialtest/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.jyheo.materialtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ContactsAdapter adapter = new ContactsAdapter(this, contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    ArrayList<Contact> contacts;
+
+
+}
+
+
