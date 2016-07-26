43578481a2f9b81e5cfe8242ba9d5941c1917aa9
Junyoung Heo <jyheo0@gmail.com>
2016-07-27 00:59:49 +0900

    firebase storage download example

--- FirebaseTest/app/src/main/java/com/example/jyheo/firebasetest/MainActivity.java
+++ FirebaseTest/app/src/main/java/com/example/jyheo/firebasetest/MainActivity.java
@@ -1,23 +1,31 @@
 package com.example.jyheo.firebasetest;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = "MainActivity";
     private FirebaseAuth mAuth;
+    private FirebaseStorage mFirebaseStorage;
     private FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
@@ -31,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         if (user == null)
             finish();
 
+        mFirebaseStorage = FirebaseStorage.getInstance();
         mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
 
         // Create Remote Config Setting to enable developer mode.
@@ -45,6 +54,28 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseRemoteConfig.setDefaults(R.xml.remote_config_defaults);
 
         displayConfig();
+        displayImage();
+    }
+
+    private void displayImage() {
+        // Create a storage reference from our app
+        StorageReference storageRef = mFirebaseStorage.getReferenceFromUrl("gs://myfirebase-332e8.appspot.com/3.jpg");
+        storageRef.getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Log.d(TAG, "getBytes Success");
+                // Use the bytes to display the image
+                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                ImageView iv = (ImageView)findViewById(R.id.imageView);
+                iv.setImageBitmap(bmp);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+                Log.d(TAG, "getBytes Failed");
+            }
+        });
     }
 
     private void displayConfig() {
