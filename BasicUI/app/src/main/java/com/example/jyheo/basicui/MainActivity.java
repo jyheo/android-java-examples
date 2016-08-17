e0985523d047b33960c2ad5b4e771b586f40d7fc
Junyoung Heo <jyheo0@gmail.com>
2016-08-18 03:30:23 +0900

    button click

--- BasicUI/app/src/main/java/com/example/jyheo/basicui/MainActivity.java
+++ BasicUI/app/src/main/java/com/example/jyheo/basicui/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.jyheo.basicui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), R.string.button_clicked_msg,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
