8befc3d16c024bb0d8f2f3edb098e21e13ee9f59
Junyoung Heo <jyheo0@gmail.com>
2016-08-19 14:37:24 +0900

    event handler

--- BasicUI/app/src/main/java/com/example/jyheo/basicui/MainActivity.java
+++ BasicUI/app/src/main/java/com/example/jyheo/basicui/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.jyheo.basicui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), R.string.button_clicked_msg,
                         Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn = (Button)findViewById(R.id.button4);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button btn = (Button)findViewById(R.id.button1);
+                if (btn.getText().toString() == "Button 1")
+                    btn.setText("Button One");
+                else
+                    btn.setText("Button 1");
             }
         });
     }
