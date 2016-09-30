a53720024980c7b23ce94002d5c1661f535a6e43
Junyoung Heo <jyheo0@gmail.com>
2016-09-30 15:24:54 +0900

    remove unused import

--- NavigationUI/app/src/main/java/com/example/jyheo/navigationui/MainActivity.java
+++ NavigationUI/app/src/main/java/com/example/jyheo/navigationui/MainActivity.java
@@ -2,9 +2,7 @@ package com.example.jyheo.navigationui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
