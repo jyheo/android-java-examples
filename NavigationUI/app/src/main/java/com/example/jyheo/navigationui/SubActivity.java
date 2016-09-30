3309c96017c9d9d380f855aeee6c2b2ba4427fea
Junyoung Heo <jyheo0@gmail.com>
2016-09-30 15:11:28 +0900

    drawable tint

--- NavigationUI/app/src/main/java/com/example/jyheo/navigationui/SubActivity.java
+++ NavigationUI/app/src/main/java/com/example/jyheo/navigationui/SubActivity.java
@@ -1,5 +1,7 @@
 package com.example.jyheo.navigationui;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +15,11 @@ public class SubActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_black_24dp);
+            Drawable drawable = getDrawable(R.drawable.ic_keyboard_arrow_left_black_24dp);
+            if (drawable != null) {
+                drawable.setTint(Color.WHITE);
+                actionBar.setHomeAsUpIndicator(drawable);
+            }
         }
     }
 }
