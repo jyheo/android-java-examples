c54be79b42c294df8f7d964fcfac55c3b3489a60
Junyoung Heo <jyheo0@gmail.com>
2017-03-23 00:29:26 +0900

    content resolver example. call log

---
+++ ContentResolverEx/app/src/main/java/com/example/jyheo/contentresolverex/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.jyheo.contentresolverex;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.CallLog;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    final int REQUEST_CODE_PERM_READ_CALL_LOG = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED) {
+            updateCallLog();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_CALL_LOG}, REQUEST_CODE_PERM_READ_CALL_LOG);
+        }
+
+    }
+
+    private void updateCallLog() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG) != PackageManager.PERMISSION_GRANTED)
+            return;
+        Cursor cursor = getContentResolver().query(CallLog.Calls.CONTENT_URI, new String[]{CallLog.Calls.NUMBER}, null, null, null);
+        int number_index = cursor.getColumnIndex(CallLog.Calls.NUMBER);
+        ArrayList<String> call_list = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            String num = cursor.getString(number_index);
+            call_list.add(num);
+        }
+        cursor.close();
+
+        ListView lv = (ListView)findViewById(R.id.calllog);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                call_list);
+        lv.setAdapter(adapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERM_READ_CALL_LOG) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                updateCallLog();
+            } else {
+                // permission for READ_CALL_LOG rejected!
+            }
+        }
+    }
+}
