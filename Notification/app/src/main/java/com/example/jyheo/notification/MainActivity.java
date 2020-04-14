ba569cbfac16a9960b51941efb5d4562ea037b25
Junyoung Heo <jyheo0@gmail.com>
2020-04-14 23:08:23 +0900

    notificationmangercompat

--- Notification/app/src/main/java/com/example/jyheo/notification/MainActivity.java
+++ Notification/app/src/main/java/com/example/jyheo/notification/MainActivity.java
@@ -1,43 +1,58 @@
 package com.example.jyheo.notification;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static int MY_NOTIFICATION_ID = 1;
+    final private String CHANNEL_ID = "default";
+    private NotificationManagerCompat mNotificationManagerCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNotificationManagerCompat = NotificationManagerCompat.from(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // API 26
+            mNotificationManagerCompat.createNotificationChannel(
+                    new NotificationChannel(CHANNEL_ID, "default channel",
+                            NotificationManager.IMPORTANCE_DEFAULT));
+        }
     }
 
     public void onSimpleNotification(View v) {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        if (!mNotificationManagerCompat.areNotificationsEnabled())
+            return;
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
         mBuilder.setSmallIcon(R.drawable.ic_alarm_on_black_24dp);
         mBuilder.setContentTitle(getResources().getString(R.string.notif_title));
         mBuilder.setContentText(getResources().getString(R.string.notif_body));
 
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         // MY_NOTIFICATION_ID allows you to update the notification later on.
-        mNotificationManager.notify(MY_NOTIFICATION_ID, mBuilder.build());
+        mNotificationManagerCompat.notify(MY_NOTIFICATION_ID, mBuilder.build());
     }
 
     public void onActionNotificationSpecial(View v) {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        if (!mNotificationManagerCompat.areNotificationsEnabled())
+            return;
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
         mBuilder.setSmallIcon(R.drawable.ic_alarm_on_black_24dp);
         mBuilder.setContentTitle(getResources().getString(R.string.notif_title));
         mBuilder.setContentText(getResources().getString(R.string.notif_body));
@@ -50,12 +65,13 @@ public class MainActivity extends AppCompatActivity {
         mBuilder.setContentIntent(pIntent);
         mBuilder.setAutoCancel(true);
 
-        ((NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE))
-                .notify(MY_NOTIFICATION_ID, mBuilder.build());
+        mNotificationManagerCompat.notify(MY_NOTIFICATION_ID, mBuilder.build());
     }
 
     public void onActionNotificationRegular(View v) {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        if (!mNotificationManagerCompat.areNotificationsEnabled())
+            return;
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
         mBuilder.setSmallIcon(R.drawable.ic_alarm_on_black_24dp);
         mBuilder.setContentTitle(getResources().getString(R.string.notif_title));
         mBuilder.setContentText(getResources().getString(R.string.notif_body));
@@ -71,12 +87,13 @@ public class MainActivity extends AppCompatActivity {
         mBuilder.setContentIntent(pIntent);
         mBuilder.setAutoCancel(true);
 
-        ((NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE))
-                .notify(MY_NOTIFICATION_ID, mBuilder.build());
+        mNotificationManagerCompat.notify(MY_NOTIFICATION_ID, mBuilder.build());
     }
 
     public void onActionButtonNotification(View v) {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        if (!mNotificationManagerCompat.areNotificationsEnabled())
+            return;
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
         mBuilder.setSmallIcon(R.drawable.ic_alarm_on_black_24dp);
         mBuilder.setContentTitle(getResources().getString(R.string.notif_title));
         mBuilder.setContentText(getResources().getString(R.string.notif_body));
@@ -87,13 +104,13 @@ public class MainActivity extends AppCompatActivity {
                 0, callintent, PendingIntent.FLAG_CANCEL_CURRENT);
         mBuilder.addAction(R.drawable.ic_phone_black_24dp, "Call", pIntent);
 
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(MY_NOTIFICATION_ID, mBuilder.build());
+        mNotificationManagerCompat.notify(MY_NOTIFICATION_ID, mBuilder.build());
     }
 
     public void onExpandableNotification(View v) {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        if (!mNotificationManagerCompat.areNotificationsEnabled())
+            return;
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
         mBuilder.setSmallIcon(R.drawable.ic_alarm_on_black_24dp);
         mBuilder.setContentTitle(getResources().getString(R.string.notif_title));
         mBuilder.setContentText(getResources().getString(R.string.notif_body));
@@ -109,20 +126,18 @@ public class MainActivity extends AppCompatActivity {
                 new NotificationCompat.BigPictureStyle().bigPicture(largeIcon);
         mBuilder.setStyle(bpStyle);
 
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(MY_NOTIFICATION_ID, mBuilder.build());
+        mNotificationManagerCompat.notify(MY_NOTIFICATION_ID, mBuilder.build());
     }
 
     public void onProgressNotification(View v) {
-        final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        if (!mNotificationManagerCompat.areNotificationsEnabled())
+            return;
+        final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
         mBuilder.setSmallIcon(R.drawable.ic_alarm_on_black_24dp);
         mBuilder.setContentTitle("Download");
         mBuilder.setContentText("Download in progress");
         mBuilder.setAutoCancel(true);
 
-        final NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         new Thread(
                 new Runnable() {
                     @Override
@@ -130,7 +145,7 @@ public class MainActivity extends AppCompatActivity {
                         int incr;
                         for (incr = 0; incr <= 100; incr += 5) {
                             mBuilder.setProgress(100, incr, false);
-                            mNotificationManager.notify(MY_NOTIFICATION_ID, mBuilder.build());
+                            mNotificationManagerCompat.notify(MY_NOTIFICATION_ID, mBuilder.build());
                             try {
                                 Thread.sleep(5 * 1000); // Sleep for 5 seconds
                             } catch (InterruptedException e) {
@@ -139,7 +154,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                         mBuilder.setContentText("Download complete")
                                 .setProgress(0, 0, false); // Removes the progress bar
-                        mNotificationManager.notify(MY_NOTIFICATION_ID, mBuilder.build());
+                        mNotificationManagerCompat.notify(MY_NOTIFICATION_ID, mBuilder.build());
                     }
                 }
         ).start();
--- Notification/app/src/main/java/com/example/jyheo/notification/SecondActivity.java
+++ Notification/app/src/main/java/com/example/jyheo/notification/SecondActivity.java
@@ -1,7 +1,7 @@
 package com.example.jyheo.notification;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
